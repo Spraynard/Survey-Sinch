8d886bb203dd5333fef84956f94616976319eed9
Kellan Martin <kmartin@vandykmortgage.com>
2020-06-09 17:03:30 -0400

    Setting up SurveyGenerator to allow for one level of nesting

--- src/SurveyorGenerator.tsx
+++ src/SurveyorGenerator.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { SurveyorSurvey, SurveyComponentState, SurveyComponentItem, SurveyComponentRef } from "./types";
+import { SurveyorSurvey, SurveyComponentState, SurveyComponentItem, SurveyComponentRefObject } from "./types";
 import { SurveyorFactory } from "./SurveyorFactory";
 import { makeSurveyComponentState, flatten } from "./functions";
 import { isArray } from "util";
@@ -19,7 +19,7 @@ type InputProps = {
     component : ISurveyComponent | IChoicedSurveyComponent;
     value : any;
     onUpdate : (event) => void;
-    forwardedRef : SurveyComponentRef;
+    forwardedRef : SurveyComponentRefObject;
 }
 
 const SurveyorComponent = ({ component, value, forwardedRef, onUpdate } : InputProps) => {
@@ -35,32 +35,39 @@ const SurveyorComponent = ({ component, value, forwardedRef, onUpdate } : InputP
 }
 
 
-const SurveyorGenerator = React.forwardRef(({ current_question, survey_data, surveyState, setSurveyState, depth = 0 } : Props, ref: SurveyComponentRef) => {
+const SurveyorGenerator = React.forwardRef(({ current_question, survey_data, surveyState, setSurveyState, depth = 0 } : Props, ref: SurveyComponentRefObject) => {
     const onUpdate = ( id ) => ( event ) => setSurveyState({ ...surveyState, ...makeSurveyComponentState(id, event.target.value)})
     return <SurveyorContainer depth={depth}>
         { survey_data.map( component => {
+
+            /**
+             * Allow for one level of nesting in order to display a grouping of questions to the screen.
+             */
             if ( isArray(component) )
             {
                 let flattened_survey_data = flatten(component),
-                    survey_data_ids = flattened_survey_data.map((data) => data.id);
+                    survey_data_ids: Array<string> = flattened_survey_data.map((data) => data.id),
+                    frame_id: string = survey_data_ids.reduce((prev, current) => prev + current, "");
 
 
-                console.log(flattened_survey_data);
                 return (
-                    <SurveyorFrame display={(survey_data_ids.includes(current_question))}>
+                    <SurveyorFrame key={frame_id} showFrame={(survey_data_ids.includes(current_question))}>
                         { component.map( item => {
-                            return <SurveyorComponent component={item} forwardedRef={ref} value={surveyState[item.id].value} onUpdate={onUpdate(item.id)}/>
+                            return <SurveyorComponent key={item.id} component={item} forwardedRef={ref} value={surveyState[item.id].value} onUpdate={onUpdate(item.id)}/>
                          })
                         }
                     </SurveyorFrame>
                 )
             }
-
+            
+            /**
+             * Display a single question element to the screen.
+             */
             const { id } = component;
 
             return (
-                <SurveyorFrame display={id === current_question}>
-                    <SurveyorComponent component={component} forwardedRef={ref} value={surveyState[id].value} onUpdate={onUpdate(id)}/>
+                <SurveyorFrame key={id} showFrame={id === current_question}>
+                    <SurveyorComponent key={id} component={component} forwardedRef={ref} value={surveyState[id].value} onUpdate={onUpdate(id)}/>
                 </SurveyorFrame>
             )
         }) }
