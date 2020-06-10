7dbd5bc294d64fdd7d018f72d945f152db26dd4f
Kellan Martin <kmartin@vandykmortgage.com>
2020-06-10 19:39:26 -0400

    Removing Unneeded Files

--- src/components/SurveyorCheckboxGroup.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { SurveyComponentItem } from "../types";
-
-
-const SurveyorCheckbox = ({ label, value }: SurveyComponentItem) =>
-    <input type="checkbox" value={value}>label</input>
-export const SurveyorCheckboxGroup = ({ items }) => {
-
-}
