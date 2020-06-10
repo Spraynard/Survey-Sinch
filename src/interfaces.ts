1a82ca97d65d0fabc61deadaad74f57320bd5381
Kellan Martin <kmartin@vandykmortgage.com>
2020-06-10 19:43:15 -0400

    Updating Interfaces.ts adding space in ISurveyComponent label component

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -3,7 +3,7 @@ import { SurveyComponentItem } from "./types";
 export interface ISurveyComponent {
     id?: string;
     type : string, // The Type of component it is
-    label: string
+    label : string
 }
 
 export interface IChoicedSurveyComponent extends ISurveyComponent {
