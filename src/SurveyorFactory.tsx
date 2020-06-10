a20068753a8b1f49ea71d8c23c635b86122ec0b1
Kellan Martin <kmartin@vandykmortgage.com>
2020-06-10 19:43:51 -0400

    Modifying SurveyorFactory
    Adding in new survey elements for the factory to generate

--- src/SurveyorFactory.tsx
+++ src/SurveyorFactory.tsx
@@ -1,42 +1,53 @@
 import React from "react";
 
-import { ISurveyComponent } from "./interfaces";
+import { ISurveyComponent, IChoicedSurveyComponent } from "./interfaces";
 import { SurveyComponentType, SurveyComponentRef, SurveyComponentRefObject } from "./types";
+import { InputElement, ElementGroup } from "./components/InputComponents";
 
 type Props = {
-    component : ISurveyComponent;
-    value : string;
-    onUpdate : React.ChangeEventHandler;
-    forwardedRef : SurveyComponentRefObject;
+    component: ISurveyComponent | IChoicedSurveyComponent;
+    value: string;
+    onUpdate: React.ChangeEventHandler;
+    focusHandler: (event) => void;
+    forwardedRef: SurveyComponentRefObject;
 }
 
-const SurveyorFactory = ({ component, onUpdate, value, forwardedRef } : Props) => {
+
+const SurveyorFactory = ({ component, focusHandler, onUpdate, value, forwardedRef }: Props) => {
     const type = component.type as SurveyComponentType;
 
-    /**
-     * Casting the values of the ref to the specific types that they need to be
-     */
-    switch ( type ) {
+    switch (type) {
         case "text":
-            return <input
-                id={component.id}
-                ref={forwardedRef.current[component.id] as React.MutableRefObject<HTMLInputElement> | null} type="text" value={value} onChange={onUpdate}/>
         case "number":
-            return <input
+            return <InputElement
                 id={component.id}
-                ref={forwardedRef.current[component.id] as React.MutableRefObject<HTMLInputElement> | null} type="number" value={value}/>
+                forwardedRef={forwardedRef}
+                value={value}
+                type={type}
+                onUpdate={onUpdate}
+                focusHandler={focusHandler}
+                labelText={component.label}
+            />
         case "textbox":
             return <textarea
                 id={component.id}
-                ref={forwardedRef.current[component.id] as React.MutableRefObject<HTMLTextAreaElement> | null}>{value}</textarea>
+                ref={forwardedRef.current[component.id] as React.MutableRefObject<HTMLTextAreaElement>}>{value}</textarea>
         case "select":
             return <select
                 id={component.id}
-                ref={forwardedRef.current[component.id] as React.MutableRefObject<HTMLSelectElement> | null}>{value}</select>
+                ref={forwardedRef.current[component.id] as React.MutableRefObject<HTMLSelectElement>}>{value}</select>
         case "radio":
-            return <input
+        case "checkbox":
+            return <ElementGroup
                 id={component.id}
-                ref={forwardedRef.current[component.id] as React.MutableRefObject<HTMLInputElement> | null} type="radio"/>
+                current_value={value}
+                heading={component.label}
+                items={(component as IChoicedSurveyComponent).items}
+                forwardedRef={forwardedRef} 
+                type={type}
+                onUpdate={onUpdate}
+                focusHandler={focusHandler}
+            />
         default:
             throw new Error(`Error: Unknown Component Type ${component.type}`)
     }
