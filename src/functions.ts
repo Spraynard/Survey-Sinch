fe0b62de06e995e85b55d384908e8944f7be5668
Kellan Martin <kmartin@vandykmortgage.com>
2020-06-11 09:37:56 -0400

    Modifying functions.ts
    Adding in focus and update event handlers

--- src/functions.ts
+++ src/functions.ts
@@ -6,7 +6,7 @@ import { v4 as uuid_generator } from "uuid";
  * Given a "Key" string from a synthetic event, be true if it is the space key.
  * @param key_string Synthetic Event "Key" string
  */
-function isSpaceKey( key_string: string ): boolean {
+function isSpaceKey(key_string: string): boolean {
     return key_string === " ";
 }
 
@@ -14,7 +14,7 @@ function isSpaceKey( key_string: string ): boolean {
  * Returns the first item in a list
  * @param items Array of arbitrary items
  */
-function first<T>( items : Array<T> ) : T {
+function first<T>(items: Array<T>): T {
     return items[0];
 }
 
@@ -22,7 +22,7 @@ function first<T>( items : Array<T> ) : T {
  * Returns all but the first item in a list
  * @param items Array of arbitrary items
  */
-function rest<T>( items : Array<T> ) : Array<T> {
+function rest<T>(items: Array<T>): Array<T> {
     return items.slice(1);
 }
 
@@ -30,14 +30,13 @@ function rest<T>( items : Array<T> ) : Array<T> {
  * Turn a multi-dimensional array into single-dimension
  * @param items Array of items to be flattened.
  */
-function flatten( items : Array<any> ) : any[] {
-    return items.reduce(( acc, curr ) => {
-        if ( ! isArray( curr ) )
-        {
-            return acc.concat( curr );
+function flatten<T>(items: Array<T>): Array<T> {
+    return items.reduce((acc, curr) => {
+        if (!isArray(curr)) {
+            return acc.concat(curr);
         }
 
-        return acc.concat(flatten( curr ) );
+        return acc.concat(flatten(curr));
     }, [])
 }
 
@@ -46,37 +45,90 @@ function flatten( items : Array<any> ) : any[] {
  * @param id UUID String
  * @param value Value for State
  */
-function makeSurveyComponentState( id: string, value: string ) : SurveyComponentState {
+function makeSurveyComponentState(id: string, value: string): SurveyComponentState {
     return {
-        [ id ] : {
+        [id]: {
             value,
-            touched : false,
+            touched: false,
         }
     }
 }
 
-async function transformComponentGroup(item : SurveyComponentGroup ):  Promise<[ SurveyComponentState, SurveyComponentGroup ]> {
+async function transformComponentGroup(item: SurveyComponentGroup): Promise<[SurveyComponentState, SurveyComponentGroup]> {
 
     let state_object = {}
 
     const transformGroup = (item) => {
         return item.map((survey_component) => {
-            if ( isArray(survey_component) )
-            {
+            if (isArray(survey_component)) {
                 transformComponentGroup(survey_component)
             }
-    
+
             const id = uuid_generator();
 
-            state_object = { ...state_object, ...makeSurveyComponentState( id, "" ) }
-            return { id, ...survey_component}
+            state_object = { ...state_object, ...makeSurveyComponentState(id, "") }
+            return { id, ...survey_component }
         })
     }
 
-    let transformed_group = await transformGroup( item );
+    const transformed_group = await transformGroup(item);
 
-    return [ state_object, transformed_group ];
+    return [state_object, transformed_group];
 }
+
+/**
+ * Global survey item "onUpdate" handler. Provides a general method of taking
+ * the value of the item that has been interacted with and saving it within
+ * state.
+ * 
+ * @param surveyState Survey State Object Getter Hook
+ * @param setSurveyState Survey State Object Setter Hook
+ */
+function onSurveyorElementUpdate(
+    surveyState: SurveyComponentState,
+    setSurveyState: React.Dispatch<React.SetStateAction<SurveyComponentState>>
+) {
+    return (id: string) => (event: React.ChangeEvent<HTMLInputElement>): void => {
+        return setSurveyState({
+            ...surveyState,
+            [id]: {
+                ...surveyState[id],
+                value: event.target.value
+            }
+        })
+    }
+}
+
+/**
+ * Global survey item "onFocus" event handler. Handles side effects of focusing
+ * for all elements that are created.
+ * 
+ * @param surveyState Surrvey state object getter hook
+ * @param setSurveyState Survey state object setter hook
+ * @param setCurrentQuestionID Setter Hook for the current question id
+ */
+function onSurveyorElementFocus(
+    surveyState: SurveyComponentState,
+    setSurveyState: React.Dispatch<React.SetStateAction<SurveyComponentState>>,
+    setCurrentQuestionID: React.Dispatch<React.SetStateAction<string>>
+) {
+    return (id: string) => (): void => {
+        setCurrentQuestionID(id);
+
+        if (surveyState[id].touched) {
+            return;
+        }
+
+        return setSurveyState({
+            ...surveyState,
+            [id]: {
+                ...surveyState[id],
+                touched: true
+            }
+        })
+    }
+}
+
 /**
  * Generates a pair that includes state to use for our generated survey (keyed by a programmatically created id) and
  * survey data transformed to include the programmatically created id.
@@ -85,35 +137,33 @@ async function transformComponentGroup(item : SurveyComponentGroup ):  Promise<[
  * @param state_object Survey State
  * @param transformed_data Initial JSON supplied survey data that is transformed to include UUIDs
  */
-async function generateSurveyorData( survey_data : SurveyorSurvey, state_object = {}, transformed_data = [] ) : Promise<[ SurveyComponentState, SurveyorSurvey ]> {
-    if ( ! survey_data.length )
-    {
-        return [ state_object, transformed_data ];
+async function generateSurveyorData(survey_data: SurveyorSurvey, state_object = {}, transformed_data = []): Promise<[SurveyComponentState, SurveyorSurvey]> {
+    if (!survey_data.length) {
+        return [state_object, transformed_data];
     }
 
-    const item: SurveyComponent = first<SurveyComponent>( survey_data ),
-          rest_items: SurveyorSurvey = rest<SurveyComponent>( survey_data );
-    
+    const item: SurveyComponent = first<SurveyComponent>(survey_data),
+        rest_items: SurveyorSurvey = rest<SurveyComponent>(survey_data);
+
     /**
      * If our item is an array, recursively generate another array.
      */
-    if ( isArray( item ) )
-    {
-        const [ state_from_group, transformed_group_data ] = await transformComponentGroup(item);
+    if (isArray(item)) {
+        const [state_from_group, transformed_group_data] = await transformComponentGroup(item);
 
-        return generateSurveyorData( 
-            rest_items, 
-            { ...state_object, ...state_from_group }, 
-            [ ...transformed_data, transformed_group_data]
+        return generateSurveyorData(
+            rest_items,
+            { ...state_object, ...state_from_group },
+            [...transformed_data, transformed_group_data]
         )
     }
 
     const item_id = uuid_generator();
 
-    return generateSurveyorData( 
-        rest_items, 
-        { ...state_object, ...makeSurveyComponentState( item_id, "" ) },
-        [ ...transformed_data, { ...item, id : item_id }]
+    return generateSurveyorData(
+        rest_items,
+        { ...state_object, ...makeSurveyComponentState(item_id, "") },
+        [...transformed_data, { ...item, id: item_id }]
     );
 }
 
@@ -123,13 +173,23 @@ async function generateSurveyorData( survey_data : SurveyorSurvey, state_object 
  * @param { SurveyComponentState } surveyState Current survey state
  * @returns { number } Percentage decimal
  */
-function calculateSurveyProgress( surveyState: SurveyComponentState ) {
-    let rawStateValues = Object.values(surveyState),
-        filteredStateValues = rawStateValues.filter(( stateValue ) => {
-            return ( stateValue.touched && stateValue.value !== "" )
+function calculateSurveyProgress(surveyState: SurveyComponentState) : number {
+    const rawStateValues = Object.values(surveyState),
+        filteredStateValues = rawStateValues.filter((stateValue) => {
+            return (stateValue.touched && stateValue.value !== "")
         });
 
-    return ( filteredStateValues.length / rawStateValues.length ) * 100;
+    return (filteredStateValues.length / rawStateValues.length) * 100;
 }
 
-export { first, rest, flatten, makeSurveyComponentState, generateSurveyorData, isSpaceKey, calculateSurveyProgress };
+export {
+    first,
+    rest,
+    flatten,
+    makeSurveyComponentState,
+    generateSurveyorData,
+    isSpaceKey,
+    calculateSurveyProgress,
+    onSurveyorElementFocus,
+    onSurveyorElementUpdate
+};
