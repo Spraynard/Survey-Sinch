964f7016249d9fcdb15fd69c5ee7a604e6f386cf
Kellan Martin <kmartin@vandykmortgage.com>
2020-06-11 10:15:16 -0400

    Modifying SurveyorGenerator.tsx
    Moving inner SurveyorGenerator functionality to another named function
    
    SurveyorGenerator is now an explicit call to React.ForwardRef to SurveyorGeneratorInner

--- src/SurveyorGenerator.tsx
+++ src/SurveyorGenerator.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { SurveyorSurvey, SurveyComponentState, SurveyComponentItem, SurveyComponentRefObject } from "./types";
+import { SurveyorSurvey, SurveyComponentState, SurveyComponentRefObject, SurveyorInputChangeHandler, SurveyorInputFocusHandler } from "./types";
 import { flatten } from "./functions";
 import { isArray } from "util";
 import { SurveyorContainer } from "./SurveyorContainer";
@@ -10,66 +10,61 @@ type Props = {
     current_question_id: string;
     survey_data: SurveyorSurvey;
     surveyState: SurveyComponentState;
-    setSurveyState: React.Dispatch<React.SetStateAction<SurveyComponentState>>;
-    depth?: number;
+    onUpdateHandler : SurveyorInputChangeHandler;
+    onFocusHandler : SurveyorInputFocusHandler;
 }
 
-const SurveyorGenerator = React.forwardRef(({ current_question_id, survey_data, surveyState, setSurveyState, depth = 0 }: Props, ref: SurveyComponentRefObject) => {
-    const onUpdate = (id: string) => (event) => {
-        return setSurveyState({
-            ...surveyState,
-            [id]: {
-                ...surveyState[id],
-                value: event.target.value
+const SurveyorGeneratorInner = ({ current_question_id, onUpdateHandler, onFocusHandler, survey_data, surveyState }: Props, ref: SurveyComponentRefObject): JSX.Element => {
+
+    return <SurveyorContainer>
+        {survey_data.map(component => {
+            /**
+             * Allow for one level of nesting in order to display a grouping of questions to the screen.
+             */
+            if (isArray(component)) {
+                const flattened_survey_data = flatten(component),
+                    survey_data_ids: Array<string> = flattened_survey_data.map((data) => data.id),
+                    frame_id: string = survey_data_ids.reduce((prev, current) => prev + current, "");
+
+
+                return (
+                    <SurveyorFrame key={frame_id} show_frame={(survey_data_ids.includes(current_question_id))}>
+                        {
+                            component.map(item => <SurveyorFactory 
+                                    key={item.id} 
+                                    component={item} 
+                                    forwardedRef={ref} 
+                                    value={surveyState[item.id].value}
+                                    focusHandler={onFocusHandler(item.id)}
+                                    onUpdate={onUpdateHandler(item.id)} 
+                                />
+                            )
+                        }
+                    </SurveyorFrame>
+                )
             }
-        })
-    },
-    focusHandler = (id: string) => (event) => {
-        if ( surveyState[id].touched )
-        {
-            return;
-        }
-        
-        return setSurveyState({
-            ...surveyState,
-            [id]: {
-                ...surveyState[id],
-                touched: true
-            }
-        })
-    };
 
-return <SurveyorContainer depth={depth}>
-    {survey_data.map(component => {
-
-        /**
-         * Allow for one level of nesting in order to display a grouping of questions to the screen.
-         */
-        if (isArray(component)) {
-            let flattened_survey_data = flatten(component),
-                survey_data_ids: Array<string> = flattened_survey_data.map((data) => data.id),
-                frame_id: string = survey_data_ids.reduce((prev, current) => prev + current, "");
-
+            /**
+             * Display a single question element to the screen.
+             */
+            const { id } = component;
 
             return (
-                <SurveyorFrame key={frame_id} show_frame={(survey_data_ids.includes(current_question_id))}>
-                    {component.map(item => <SurveyorFactory key={item.id} component={item} forwardedRef={ref} value={surveyState[item.id].value} focusHandler={focusHandler(item.id)} onUpdate={onUpdate(item.id)} />)}
+                <SurveyorFrame key={id} show_frame={id === current_question_id}>
+                    <SurveyorFactory 
+                        key={id} 
+                        component={component} 
+                        forwardedRef={ref} 
+                        value={surveyState[id].value}
+                        focusHandler={onFocusHandler(id)}
+                        onUpdate={onUpdateHandler(id)}
+                    />
                 </SurveyorFrame>
             )
-        }
+        })}
+    </SurveyorContainer>
+}
 
-        /**
-         * Display a single question element to the screen.
-         */
-        const { id } = component;
-
-        return (
-            <SurveyorFrame key={id} show_frame={id === current_question_id}>
-                <SurveyorFactory key={id} component={component} forwardedRef={ref} value={surveyState[id].value} focusHandler={focusHandler(id)} onUpdate={onUpdate(id)} />
-            </SurveyorFrame>
-        )
-    })}
-</SurveyorContainer>
-})
+const SurveyorGenerator = React.forwardRef(SurveyorGeneratorInner)
 
 export { SurveyorGenerator };
