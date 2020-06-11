689e734c349e21400d664a4a661d4a4eb420bf38
Kellan Martin <kmartin@vandykmortgage.com>
2020-06-11 14:26:14 -0400

    Updating functions.test.ts

--- test/functions.test.ts
+++ test/functions.test.ts
@@ -1,10 +1,10 @@
 import { first, rest, flatten, generateSurveyorData } from "../src/functions";
-import { SurveyorSurvey } from "../src/types";
+import { SurveySinchSurvey } from "../src/types";
 
 
-let test_arr = [ 1, 2, 3, 4 ];
+const test_arr = [ 1, 2, 3, 4 ];
 
-let test_survey: SurveyorSurvey = [
+const test_survey: SurveySinchSurvey = [
     { label : "Test #1", type : "text" },
     { label : "Test #2", type : "number" },
     [
@@ -20,19 +20,19 @@ let test_survey: SurveyorSurvey = [
 ]
 
 test("Obtains the first element of an array", () => {
-    let expected = 1;
+    const expected = 1;
 
     expect(first<number>(test_arr)).toBe(expected);
 });
 
 test("Obtains every element except the first element of an array", () => {
-    let expected = [2, 3, 4];
+    const expected = [2, 3, 4];
 
     expect(rest<number>(test_arr)).toEqual(expected);
 })
 
 test("Flatten works as expected", () => {
-    let test_flatten_array = [
+    const test_flatten_array = [
         1,
         2,
         [ 
@@ -51,16 +51,16 @@ test("Flatten works as expected", () => {
 
 test("Creates a state object with the correct number of keyed objects for each Survey Component", async () => {
 
-    let [ test_survey_state, transformed_test_survey ] = await generateSurveyorData( test_survey );
+    const [ test_survey_state, ] = await generateSurveyorData( test_survey );
     
     expect( Object.keys(test_survey_state).length ).toEqual(3);
 });
 
 test("IDs in transformed survey match IDs in state object", async () => {
 
-    let [ test_survey_state, transformed_test_survey ] = await generateSurveyorData( test_survey );
+    const [ test_survey_state, transformed_test_survey ] = await generateSurveyorData( test_survey );
     
-    let transformed_survey_ids = flatten(transformed_test_survey).map( item => item.id ),
+    const transformed_survey_ids = flatten(transformed_test_survey).map( item => item.id ),
         survey_state_ids = Object.keys(test_survey_state);
 
     expect(survey_state_ids).toEqual(transformed_survey_ids);
